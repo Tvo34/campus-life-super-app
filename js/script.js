398c881defad7eeae5ebef9dbb5c052b744cda3b
Tvo34 <lvo@northpark.edu>
2026-04-26 18:24:15 -0500

    Add Pictures and API

--- js/script.js
+++ js/script.js
@@ -5,17 +5,34 @@ function loadEvents() {
   document.getElementById("eventList").innerHTML = output;
 }
 
-// ===== MENU =====
+// ==== LOAD MENU ====
 function loadMenu() {
-  let menu = ["Pizza", "Burger", "Salad"];
-  let output = menu.map(m => "<li>" + m + "</li>").join("");
-  document.getElementById("menuList").innerHTML = output;
+  fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=chicken")
+    .then(res => res.json())
+    .then(data => {
+      let output = data.meals.slice(0, 5).map(meal => {
+        return `<li>${meal.strMeal}</li>`;
+      }).join("");
+
+      document.getElementById("menuList").innerHTML = output;
+    })
+    .catch(() => {
+      document.getElementById("menuList").innerHTML =
+        "<li>Failed to load menu</li>";
+    });
 }
 
 // ===== LOCATION =====
 function showLocation() {
-  document.getElementById("locationText").textContent =
-    "Your location: Campus Center (placeholder)";
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(pos => {
+      const lat = pos.coords.latitude;
+      const lon = pos.coords.longitude;
+
+      document.getElementById("locationText").textContent =
+        `Latitude: ${lat}, Longitude: ${lon}`;
+    });
+  }
 }
 
 // ===== WEATHER (OPEN-METEO - NO API KEY) =====
